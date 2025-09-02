e943ac23eb744481150dda66d888d6c61526a3f5
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2025-09-02 00:24:52 -0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -19,11 +19,18 @@ docker-compose up -d
 ### URLs DO PROJETO:
 
 IP=$(curl -s checkip.amazonaws.com)
-#echo "Aguardando TOKEN (geralmente 1 min)"
-#while [ "$(docker logs datacatalog-automl-1 2>&1 | grep token | grep 127. | grep NotebookApp | wc -l)" != "1" ]; do
-#  printf "."
-#  sleep 1
-#done
+echo "Aguardando RUNNING (geralmente 1 min)"
+while [ "$(docker logs caddy-caddy-1 2>&1 | grep running | wc -l)" != "1" ]; do
+  printf "."
+  sleep 1
+done
+
+docker exec -ti caddy-caddy-1 curl -X POST localhost:2019/config/admin -H "Content-Type: application/json" -d '{"listen": "tcp/0.0.0.0:2019"}'
+echo ""
+echo " VERIFICANDO CONFIG DO CADDY:"
+echo ""
+curl localhost:2019/config
+
 echo "Caddy Pronto."
 echo ""
 
@@ -32,5 +39,7 @@ echo ""
 echo "Config OK"
 echo ""
 echo ""
-echo " - CADDY      : http://$IP:8888/          login: fiap     senha: fiap"
+echo " - CADDY MANAGER    : http://$IP:8888/          login: fiap     senha: fiap"
+echo ""
+echo " - CADDY UI    : http://$IP:8880/"
 echo ""
